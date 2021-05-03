1ccc213ffaf4b0f133f2c2faae02845672519095
unknown <Arkadiusz>
2021-05-04 01:01:10 +0200

    eeprom - basic

--- Core/Application/modules/eeprom/eeprom.c
+++ Core/Application/modules/eeprom/eeprom.c
@@ -9,28 +9,46 @@
 #include "i2c.h"
 
 bool eeprom_init(void) {
-	;
+
+	if (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_I2C_ADDRESS, EEPROM_MAX_TRIALS,
+	HAL_MAX_DELAY) != HAL_OK) {
+		return false;
+	}
+	return true;
+
+}
+
+bool eeprom_read(uint8_t *pBuffer, uint16_t ReadAddr, uint16_t *NumByteToRead) {
+
+	uint32_t buffersize = *NumByteToRead;
+
+	HAL_StatusTypeDef status = HAL_OK;
+
+	status = HAL_I2C_Mem_Read(&hi2c1, EEPROM_I2C_ADDRESS, (uint16_t) ReadAddr,
+	I2C_MEMADD_SIZE_16BIT, pBuffer, buffersize, EEPROM_I2C_TIMEOUT);
+
+	if (status != HAL_OK) {
+		return false;
+	}
+
 	return true;
 }
 
-//void start_condition(bool read_write) {
-//	uint8_t addressing_memory_array = 0b10100000;
-//	addressing_memory_array |= (read_write << 0);
-//}
+bool eeprom_write(uint8_t *pBuffer, uint16_t WriteAddr, uint16_t NumByteToWrite) {
 
-void eeprom_write(uint8_t where[2], uint8_t *pData, uint16_t size) {
-	HAL_I2C_Master_Transmit(&hi2c1, EEPROM_MEMORY_ARRAY_ADDRESS, where, 2,
-	HAL_MAX_DELAY);
-
-	HAL_I2C_Master_Transmit(&hi2c1, EEPROM_MEMORY_ARRAY_ADDRESS, pData, size,
-	HAL_MAX_DELAY);
 }
 
-void eeprom_read(uint8_t where[2], uint8_t *received_data, uint16_t size) {
-	HAL_I2C_Master_Transmit(&hi2c1, EEPROM_MEMORY_ARRAY_ADDRESS, where, 2,
-	HAL_MAX_DELAY);
+bool eeprom_write_page(uint8_t *pBuffer, uint16_t WriteAddr,
+		uint8_t *NumByteToWrite) {
+	uint32_t buffersize = *NumByteToWrite;
 
-	HAL_I2C_Master_Receive(&hi2c1, EEPROM_MEMORY_ARRAY_ADDRESS, received_data,
-			size, HAL_MAX_DELAY);
+	HAL_StatusTypeDef status = HAL_OK;
+
+	status = HAL_I2C_Mem_Write(&hi2c1, EEPROM_I2C_ADDRESS, (uint16_t) WriteAddr,
+	I2C_MEMADD_SIZE_16BIT, pBuffer, buffersize, EEPROM_I2C_TIMEOUT);
+	if (status != HAL_OK) {
+		return false;
+	}
+
+	return true;
 }
-
--- Core/Application/modules/eeprom/eeprom.h
+++ Core/Application/modules/eeprom/eeprom.h
@@ -15,11 +15,25 @@
 #include "stdint.h"
 #include "main.h"
 
+//#define EEPROM_I2C_ADDRESS                    (0b1010000 << 1)
+#define EEPROM_I2C_ADDRESS          0xA0
+#define EEPROM_MAX_TRIALS           3000
+#define EEPROM_I2C_TIMEOUT          100
+
+//The Page Write mode allows up to 32 byte to be written in a single Write cycle
+#define EEPROM_PAGESIZE             32
+#define EEPROM_MAX_SIZE             0x2000 /* 64Kbit*/
+
+//#define EEPROM_OK                   0
+//#define EEPROM_FAIL                 1
+//#define EEPROM_TIMEOUT              2
+
 bool eeprom_init(void);
 
-//void start_condition(void);
-void eeprom_write(uint8_t where[2], uint8_t *pData, uint16_t size);
+bool eeprom_write(uint8_t *pBuffer, uint16_t WriteAddr, uint16_t NumByteToWrite);
 
-void eeprom_read(uint8_t where[2], uint8_t *received_data, uint16_t size);
+bool eeprom_read(uint8_t *pBuffer, uint16_t ReadAddr, uint16_t *NumByteToRead);
+
+bool eeprom_write_page(uint8_t* pBuffer, uint16_t WriteAddr, uint8_t* NumByteToWrite);
 
 #endif /* APPLICATION_MODULES_EEPROM_EEPROM_H_ */
